6f98716fcce88ce9190887ab9c5a6c7c13df4d27
EnzoJauregui <enzo97zj@gmail.com>
2026-06-13 23:53:53 -0300

    Cambios en rutas de imports

--- server/src/estadisticas/estadisticas.module.ts
+++ server/src/estadisticas/estadisticas.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EstadisticasService } from './estadisticas.service';
 import { EstadisticasController } from './estadisticas.controller';
-import { AutenticationModule } from '@/autentication/autentication.module';
-import { PublicacionesModule } from '@/publicaciones/publicaciones.module';
-import { ComentariosModule } from '@/comentarios/comentarios.module';
+import { AutenticationModule } from '../autentication/autentication.module';
+import { PublicacionesModule } from '../publicaciones/publicaciones.module';
+import { ComentariosModule } from '../comentarios/comentarios.module';
 
 @Module({
   imports: [
--- server/src/usuarios/usuarios.module.ts
+++ server/src/usuarios/usuarios.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
-import { AutenticationModule } from '@/autentication/autentication.module';
+import { AutenticationModule } from '../autentication/autentication.module';
 
 @Module({
   imports: [AutenticationModule],
